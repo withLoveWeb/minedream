38f8b1915bac8a4aea5cc556984988eb4470e293
NikVislov <volkagu@yandex.ru>
2022-10-08 23:29:09 +0300

    Designed

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,8 @@
-console.log('file 1');
+const copyIp = document.querySelector('.ip__copy-btn');
+
+
+
+copyIp.addEventListener('click', (event) => {
+  const copyText = document.querySelector('.ip__input').value
+  navigator.clipboard.writeText(copyText)
+})
